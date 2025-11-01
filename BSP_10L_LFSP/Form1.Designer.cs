6322da856a138fcb0c8bbf88544d3dd992657cdf
My <mymail@mail.ru>
2025-11-01 13:22:27 +0400

    Добавьте файлы проекта.

---
+++ BSP_10L_LFSP/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace BSP_10L_LFSP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(25, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(869, 356);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Расчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 413);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(129, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(235, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Начальное состояние";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(326, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Неприводимый многочлен: x^8 + x^4 + x^3 + x^2 + 1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(24, 178);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(79, 260);
+            this.listBox1.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(129, 95);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(934, 460);
+            this.panel1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1075, 567);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ BSP_10L_LFSP/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace BSP_10L_LFSP
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Bitmap bmp;
+        Pen DrawPen = new Pen(Color.Red, 5);
+        Pen PenLine = new Pen(Color.Red, 1);
+        Pen PenLineCon = new Pen(Color.Blue, 1);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void DrawPoints(uint[] mas)
+        {
+            g = pictureBox1.CreateGraphics();
+
+            int otstup_x = 32;
+            int l_otstup = 10;
+            int otstup_y = 20;
+
+            int height_picture = Convert.ToInt32(mas.Max<uint>()) + 30;
+            bmp = new Bitmap(mas.Length* otstup_x + l_otstup, height_picture);
+
+            pictureBox1.Width = bmp.Width;
+            pictureBox1.Height = height_picture;
+            g = Graphics.FromImage(bmp);
+
+            pictureBox1.Image = bmp;
+            g.Clear(Color.White);
+
+
+
+            string label_counts = "   ";
+            string label_numbers = "   ";
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+
+                label_counts += mas[i].ToString();
+                for (int j = 8 - mas[i].ToString().Length; j > 0; j--)
+                    label_counts += " ";
+
+                label_numbers += i.ToString();
+                for (int j = 9 - i.ToString().Length; j > 0; j--)
+                    label_numbers += " ";
+
+                int main_otstup = i * otstup_x + l_otstup;
+
+                g.DrawEllipse(DrawPen, main_otstup - 2, height_picture - mas[i] - otstup_y, 4, 4);
+                g.DrawLine(PenLine, new Point(main_otstup, height_picture - (int)mas[i] - otstup_y), new Point(main_otstup, height_picture));
+
+                if (i != mas.Length - 1)
+                {
+                    g.DrawLine(PenLineCon, new Point(main_otstup, height_picture - (int)mas[i] - otstup_y), new Point(main_otstup + otstup_x, height_picture - (int)mas[i+1] - otstup_y));
+                }
+            }
+            label1.Text = label_counts;
+            label2.Text = label_numbers;
+
+            pictureBox1.Refresh();
+
+        }
+
+        public class LFSRGenerator
+        {
+            private uint state;
+            private readonly uint polynomial;
+
+            public LFSRGenerator(uint seed, uint polynomial)
+            {
+                if (seed == 0)
+                    throw new ArgumentException("Seed cannot be zero", nameof(seed));
+
+                this.state = seed;
+                this.polynomial = polynomial;
+            }
+
+            public uint Next()
+            {
+                uint buff_state = state & polynomial;
+                if ((buff_state == 0b11001) || (buff_state == 0b10101) || (buff_state == 0b01101) || (buff_state == 0b00001) || (buff_state == 0b10000) || (buff_state == 0b01000) || (buff_state == 0b00100) || (buff_state == 0b11100))
+                    state = (state >> 1) | 0b10000000;//128 = 0b10000000
+                else
+                    state = state >> 1;
+
+                return state;
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // Пример неприводимого многочлена: x^8 + x^4 + x^3 + x^2 + 1 (0x1D в шестнадцатеричном виде, 29 десятичной)
+            uint polynomial = 29; // 8-битный
+
+            uint seed = Convert.ToUInt32(textBox1.Text);
+            if (seed == 0)
+            {
+                MessageBox.Show("Начальное состояние не может быть 0"); 
+                return;
+            }
+
+            var generator = new LFSRGenerator(seed, polynomial);
+
+            uint[] result = new uint[256];
+            result[0] = seed;
+
+            for (int i = 1; i < 256; i++)
+                result[i] = generator.Next();
+            DrawPoints(result);
+            FillListBox(result);
+        }
+
+        private void FillListBox(uint[] result)
+        {
+            listBox1.Items.Clear();
+            for(int i = 0; i < result.Length; i++) 
+                listBox1.Items.Add(result[i].ToString());
+        }
+    }
+}
